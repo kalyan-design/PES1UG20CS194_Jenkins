9e6486d1ffb495c2a9608b8a6a9ae3214784bbfe
kalyan-design <80830473+kalyan-design@users.noreply.github.com>
2023-02-18 23:46:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins,MY SRN is PES1UG20CS194\n";
+  cout << "Hello, Jenkins,MY  SRN is PES1UG20CS194\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
